a3cba305ca58b5a717994ce67e49380b667afc60
ayushbadgujar9 <ayushbadgujar9@gmail.com>
2026-09-15 22:39:37 +0530

    Time: 0 ms (100%), Space: 37 MB (72.52%) - LeetHub

---
+++ 3895-count-digit-appearances/3895-count-digit-appearances.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countDigitOccurrences(vector<int>& nums, int digit) {
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            while(nums[i]>0){
+                int r=nums[i]%10;
+                if(r==digit) count++;
+                nums[i]/=10;
+            }
+        }
+        return count;
+    }
+};
